2a3a15eb86f00fee318c8ba596224a69d62ea2bb
Khutaijah <khutaijah@gmail.com>
2023-06-20 13:32:22 -0700

    changes

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,14 +2,13 @@
 #include <stdio.h>
 
 /**
-* main - check the code
-*
-* Return: Always 0.
-*/
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 int n;
-
 n = add(89, 9);
 printf("%d\n", n);
 return (0);
