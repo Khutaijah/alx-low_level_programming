f08806ee5a808239e7d693858c2bb5de2bb8c1cf
Khutaijah <khutaijah@gmail.com>
2023-08-28 11:22:25 -0700

    syntax correction

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,19 +1,20 @@
+#include "lists.h"
 #include <stdio.h>
 
 /**
  * print_listint - prints all elements in a linked list
- * @h: pointer to head of list
+ * @h: linked list of type listint_t to print
  * Return: number of nodes
  */
 
-size_t print_listint(const listint_t *h);
+size_t print_listint(const listint_t *h)
 {
 	size_t count_nodes = 0;
-	while (h);
+	while (h)
 	{
 		printf("%d\n", h->n);
 		count_nodes++;
-		h = h->next
+		h = h->next;
 	}
 	return (count_nodes);
 }
